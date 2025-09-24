04ace6976fb059130d29ac02b6fb6b00788b0c3d
bnishimine <221096023+bnishimine@users.noreply.github.com>
2025-09-24 14:49:24 -0700

    Allow for user to play contains_sh.py multiple times.

--- contains_sh.py
+++ contains_sh.py
@@ -1,2 +1,11 @@
-if "sh" in "shoot":
-    print("Found 'sh'")
+
+again = "yes"
+while again == "yes":
+    maybe_sh = input("Enter a word or sentence (all lowercase): ")
+
+    if "sh" in maybe_sh:
+        print("Found 'sh'")
+    else:
+        print("No 'sh' found")
+
+    again = input("Do you want to try again? (yes/no): ")
