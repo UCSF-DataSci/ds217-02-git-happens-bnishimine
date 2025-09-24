224929a1be702a72fb25cc637109b55f5aad2d2e
bnishimine <221096023+bnishimine@users.noreply.github.com>
2025-09-24 14:54:13 -0700

    Provide locations of sh in contains_sh.py.

--- contains_sh.py
+++ contains_sh.py
@@ -3,8 +3,12 @@ again = "yes"
 while again == "yes":
     maybe_sh = input("Enter a word or sentence (all lowercase): ")
 
-    if "sh" in maybe_sh:
-        print("Found 'sh'")
+    if " sh " in maybe_sh:
+        print("Found 'sh' alone")
+    elif "sh " in maybe_sh:
+        print("Found 'sh' at the start of a word")
+    elif " sh" in maybe_sh:
+        print("Found 'sh' at the end of a word")
     else:
         print("No 'sh' found")
 
