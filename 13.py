39b3360cee54e5f7832282857b667e9245a5faeb
bnishimine <221096023+bnishimine@users.noreply.github.com>
2025-09-24 14:13:32 -0700

    Adding user interactivity to 13.py

--- 13.py
+++ 13.py
@@ -1,4 +1,8 @@
-for i in range(1, 15):
+print("Enter two values, where the first value is lower than the second value, with the number 13 in between.")
+low = int(input("Enter the lower value: "))
+high = int(input("Enter the higher value: "))
+
+for i in range(low, high):
     if i == 13:
         print(13)
     else:
