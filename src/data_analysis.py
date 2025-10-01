e398890d2f6f1160c3c46250fbb97685a78b7b21
bnishimine <221096023+bnishimine@users.noreply.github.com>
2025-10-01 15:58:03 -0700

    Update functions with functionality

--- src/data_analysis.py
+++ src/data_analysis.py
@@ -1,6 +1,14 @@
 """Basic student data analysis script."""
 
 def load_students(filename):
+    # Load student data from CSV file.
+    with open('filename', 'r') as file:
+        student_data = file.readlines()
+    students = []
+    for line in student_data:
+        students.append(line.split(","))
+    return students
+
     """Load student data from CSV file."""
     # TODO: Implement CSV loading
     pass
@@ -11,24 +19,32 @@ def calculate_average_grade(students):
     pass
 
 def count_math_students(students):
+    # Count students in Math.
+    math = students.count("Math")
+    return math
     """Count students in Math."""
     # TODO: Implement counting
     pass
 
 def generate_report(students):
-    """Generate formatted report."""
-    # TODO: Implement report generation
-    pass
+    # Generate formatted report.
+    total = len(students)
+    avg_grade = calculate_average_grade(students)
+    count_math = count_math_students(students)
+
+    return ("Number of students: ", total,
+             "\nAverage grade: ", avg_grade,
+             "\nNumber of math students: ", count_math)
+    
 
 def save_report(report, filename):
-    """Save report to file."""
-    # TODO: Implement file saving
-    pass
+    # Save report to file.
+    with open(filename, 'w') as file:
+        file.write(report)
 
 def main():
-    """Main execution function."""
-    # TODO: Orchestrate the analysis
-    pass
-
-if __name__ == "__main__":
-    main()
+    # Main execution function.
+    students = load_students('data/students.csv')
+    report = generate_report(students)
+    save_report(report, 'output/analysis_report.txt')
+    
